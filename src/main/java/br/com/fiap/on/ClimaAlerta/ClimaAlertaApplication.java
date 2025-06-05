11f35368b8969c7f444b92c7911e267cc213d36d
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2025-06-05 20:57:23 -0300

    Inicio app Spring

---
+++ src/main/java/br/com/fiap/on/ClimaAlerta/ClimaAlertaApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.on.ClimaAlerta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimaAlertaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimaAlertaApplication.class, args);
+	}
+
+}
